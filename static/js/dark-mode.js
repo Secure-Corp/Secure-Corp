bf6dbf77c4d250ef26068315f7347c051d1e508e
Carlos Arias <carlosariassnadoval34@gmail.com>
2024-06-02 23:37:26 -0600

    cositas

--- static/js/dark-mode.js
+++ static/js/dark-mode.js
@@ -1,6 +1,26 @@
-/*DARK MODE*/
 const themeSwitchCheckbox = document.querySelector(".theme-switch__checkbox");
 
+const savedTheme = localStorage.getItem('dark-mode');
+if (savedTheme) {
+    if (savedTheme === 'enabled') {
+        document.body.classList.add("active");
+        themeSwitchCheckbox.checked = true;
+    }
+} else {
+
+    const userPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    if (userPrefersDark) {
+        document.body.classList.add("active");
+        themeSwitchCheckbox.checked = true;
+    }
+}
+
 themeSwitchCheckbox.addEventListener("change", () => {
-    document.body.classList.toggle("active", themeSwitchCheckbox.checked);
+    if (themeSwitchCheckbox.checked) {
+        document.body.classList.add("active");
+        localStorage.setItem('dark-mode', 'enabled');
+    } else {
+        document.body.classList.remove("active");
+        localStorage.setItem('dark-mode', 'disabled');
+    }
 });
